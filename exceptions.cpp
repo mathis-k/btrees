27c04899c6ddc79f0d207ff13aef8b5dc7810637
Mathis Bryan Kengni <skymkengni1@gmail.com>
2024-12-25 16:44:31 +0100

    Update exceptions.cpp

--- exceptions.cpp
+++ exceptions.cpp
@@ -21,3 +21,14 @@ public:
     return message.c_str();
   }
 };
+
+class key_already_exists : public std::exception {
+private:
+  std::string message;
+public:
+  explicit key_already_exists(const std::string& msg) : message(msg) {}
+
+  const char* what() const noexcept override {
+    return message.c_str();
+  }
+};
